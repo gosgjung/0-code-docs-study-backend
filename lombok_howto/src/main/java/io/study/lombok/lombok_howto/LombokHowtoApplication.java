ea3f42bd69a3615661f593a9f211a902dd5544ff
gosgjung <gosgjung@gmail.com>
2023-03-07 05:05:31 +0900

    first commit

---
+++ lombok_howto/src/main/java/io/study/lombok/lombok_howto/LombokHowtoApplication.java
@@ -0,0 +1,13 @@
+package io.study.lombok.lombok_howto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokHowtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokHowtoApplication.class, args);
+	}
+
+}
---
+++ lombok_howto/src/test/java/io/study/lombok/lombok_howto/LombokHowtoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.lombok.lombok_howto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokHowtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

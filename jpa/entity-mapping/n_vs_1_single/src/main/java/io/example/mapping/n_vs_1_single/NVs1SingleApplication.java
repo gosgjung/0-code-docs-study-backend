d821110b60a986c68a59b6558547c4870c6d04a7
gosgjung <gosgjung@gmail.com>
2023-03-13 15:23:25 +0900

    n_vs_1_single

---
+++ jpa/entity-mapping/n_vs_1_single/src/main/java/io/example/mapping/n_vs_1_single/NVs1SingleApplication.java
@@ -0,0 +1,13 @@
+package io.example.mapping.n_vs_1_single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NVs1SingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NVs1SingleApplication.class, args);
+	}
+
+}
---
+++ jpa/entity-mapping/n_vs_1_single/src/test/java/io/example/mapping/n_vs_1_single/NVs1SingleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.mapping.n_vs_1_single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NVs1SingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

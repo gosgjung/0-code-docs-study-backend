40d1a8c74a5708143b0ca4253f37a8f99a83c37f
gosgjung <gosgjung@gmail.com>
2023-03-13 15:36:49 +0900

    프로젝트 초기세팅

---
+++ jpa/entity-mapping/n_vs_1_mirrored/src/main/java/io/example/mapping/n_vs_1_mirrored/NVs1MirroredApplication.java
@@ -0,0 +1,13 @@
+package io.example.mapping.n_vs_1_mirrored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NVs1MirroredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NVs1MirroredApplication.class, args);
+	}
+
+}
---
+++ jpa/entity-mapping/n_vs_1_mirrored/src/test/java/io/example/mapping/n_vs_1_mirrored/NVs1MirroredApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.mapping.n_vs_1_mirrored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NVs1MirroredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

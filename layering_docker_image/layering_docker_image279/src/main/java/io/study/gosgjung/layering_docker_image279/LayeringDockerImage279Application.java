d394350b4f3d75bb594d77e12eac9718dd851c9e
gosgjung <gosgjung@gmail.com>
2023-03-18 11:00:49 +0900

    docker image 레이어링

---
+++ layering_docker_image/layering_docker_image279/src/main/java/io/study/gosgjung/layering_docker_image279/LayeringDockerImage279Application.java
@@ -0,0 +1,13 @@
+package io.study.gosgjung.layering_docker_image279;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeringDockerImage279Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeringDockerImage279Application.class, args);
+	}
+
+}
---
+++ layering_docker_image/layering_docker_image279/src/test/java/io/study/gosgjung/layering_docker_image279/LayeringDockerImage279ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.gosgjung.layering_docker_image279;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeringDockerImage279ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

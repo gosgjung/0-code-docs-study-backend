61a8049d5bf7d435f05a45dada587a743335bc62
gosgjung <gosgjung@gmail.com>
2023-03-31 09:08:25 +0900

    first commit

---
+++ spring_security/security_sample_2710/src/main/java/io/sgjung/samples/security_sample_2710/SecuritySample2710Application.java
@@ -0,0 +1,13 @@
+package io.sgjung.samples.security_sample_2710;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySample2710Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySample2710Application.class, args);
+	}
+
+}
---
+++ spring_security/security_sample_2710/src/test/java/io/sgjung/samples/security_sample_2710/SecuritySample2710ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sgjung.samples.security_sample_2710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySample2710ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
